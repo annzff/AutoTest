e10347e523fd85494550c5d51ab7ce81d2ae298f
annzff <annzff@163.com>
2017-05-21 21:01:31 -0700

    add varclass.py file

---
+++ varclass.py
@@ -0,0 +1,39 @@
+class VarClass(object):
+
+    sum_name = 0
+    sum_company_name = 0
+    sum_job = 0
+    sum_mobile = 0
+    sum_telephone = 0
+    sum_address = 0
+
+    correct_name = 0
+    correct_company_name = 0
+    correct_job = 0
+    correct_mobile = 0
+    correct_telephone = 0
+    correct_address = 0
+
+
+    sum_lanmu=0
+    correct_lanmu=0
+
+    sum_chinese=0;
+    sum_notchinese=0
+
+    correct_chinese=0
+    correct_notchinese = 0
+
+    rate_correct_name = 0
+    rate_correct_company_name = 0
+    rate_correct_job = 0
+    rate_correct_mobile = 0
+    rate_correct_telephone = 0
+    rate_correct_address = 0
+    rate_lanmu=0
+    rate_chinese=0
+    rate_notchinese=0
+
+    error_lanmu={}
+
+    obj_value = []
